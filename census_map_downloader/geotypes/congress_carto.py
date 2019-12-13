d4f980327052731dc0bb9ef5b12c060addec4887
Sandhya Kambhampati <sjkambhampati@gmail.com>
2019-12-13 15:48:25 -0800

    more cleanup

--- census_map_downloader/geotypes/congress_carto.py
+++ census_map_downloader/geotypes/congress_carto.py
@@ -9,27 +9,27 @@ logger = logging.getLogger(__name__)
 
 
 class CongressCartoDownloader2018(BaseDownloader):
-	"""
-	Download 2018 cartographic congressional districts.
-	"""
-	PROCESSED_NAME = "congressionaldistricts_carto_2018"
-	# Docs (https://www2.census.gov/geo/tiger/GENZ2018/2018_file_name_def.pdf?#)
-	FIELD_CROSSWALK = collections.OrderedDict({
-		"STATEFP": "state_fip",
-		"CD116FP": "congresional_district",
-		"GEOID": "geoid",
-		"NAMELSAD": "name",
-		"geometry": "geometry",
-		"LSAD": "legal_statistical_area",
-		"CDSESSN": "congressional_session_code",
-		"ALAND": "land_area",
-		"AWATER": "water_area"
-	})
+    """
+    Download 2018 cartographic congressional districts.
+    """
+    PROCESSED_NAME = "congressionaldistricts_carto_2018"
+    # Docs (https://www2.census.gov/geo/tiger/GENZ2018/2018_file_name_def.pdf?#)
+    FIELD_CROSSWALK = collections.OrderedDict({
+        "STATEFP": "state_fip",
+        "CD116FP": "congresional_district",
+        "GEOID": "geoid",
+        "NAMELSAD": "name",
+        "geometry": "geometry",
+        "LSAD": "legal_statistical_area",
+        "CDSESSN": "congressional_session_code",
+        "ALAND": "land_area",
+        "AWATER": "water_area"
+    })
 
-	@property
-	def url(self):
-		return "https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_cd116_500k.zip"
+    @property
+    def url(self):
+        return "https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_cd116_500k.zip"
 
-	@property
-	def zip_name(self):
-		return f"cb_2018_us_cd116_500k.zip"
+    @property
+    def zip_name(self):
+        return f"cb_2018_us_cd116_500k.zip"
--- census_map_downloader/geotypes/states_carto.py
+++ census_map_downloader/geotypes/states_carto.py
@@ -9,24 +9,24 @@ logger = logging.getLogger(__name__)
 
 
 class StatesCartoDownloader2018(BaseDownloader):
-	"""
-	Download 2018 cartographic states.
-	"""
-	PROCESSED_NAME = "states_carto_2018"
-	# Docs (https://www2.census.gov/geo/tiger/GENZ2018/2018_file_name_def.pdf?#)
-	FIELD_CROSSWALK = collections.OrderedDict({
-		"STATEFP": "state_fips",
-		"GEOID": "geoid",
-		"NAME": "name",
-		"geometry": "geometry",
-		"ALAND": "land_area",
-		"AWATER": "water_area"
-	})
+    """
+    Download 2018 cartographic states.
+    """
+    PROCESSED_NAME = "states_carto_2018"
+    # Docs (https://www2.census.gov/geo/tiger/GENZ2018/2018_file_name_def.pdf?#)
+    FIELD_CROSSWALK = collections.OrderedDict({
+        "STATEFP": "state_fips",
+        "GEOID": "geoid",
+        "NAME": "name",
+        "geometry": "geometry",
+        "ALAND": "land_area",
+        "AWATER": "water_area"
+    })
 
-	@property
-	def url(self):
-		return "https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_500k.zip"
+    @property
+    def url(self):
+        return "https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_500k.zip"
 
-	@property
-	def zip_name(self):
-		return f"cb_2018_us_state_500k.zip"
+    @property
+    def zip_name(self):
+        return f"cb_2018_us_state_500k.zip"
