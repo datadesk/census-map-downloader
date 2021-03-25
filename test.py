0714e3c26b93d1df8d826afaccdcd231babdd544
Geoffrey Hing <geoffhing@gmail.com>
2021-03-25 16:45:21 +0000

    Update tests to reflect new class names
    Addresses #8
    (https://github.com/datadesk/census-map-downloader/issues/8)

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import census_map_downloader
 class CensusMapDownloadUnitTest(unittest.TestCase):
 
     def test_counties(self):
-        census_map_downloader.CountiesDownloader2018().run()
+        census_map_downloader.CountiesDownloader().run()
 
 
 if __name__ == '__main__':
